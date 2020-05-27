1bc12f94458921a57a8b9e7fa1a80870f3017f00
TiagoBrito-dev <55715873+TiagoBrito-dev@users.noreply.github.com>
2020-05-26 22:33:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,66 @@
 <?php
 
-echo"Meu primeiro codigo PHP !<b>Hello World MY Friend PEDRO, my best friend</b>";
+/*echo"Meu primeiro codigo PHP !<b>Hello World MY Friend PEDRO, my best friend</b>";
+
+echo"<br>";
+
+$curso = "Hcode";
+
+echo $curso;
+
+echo"<br>";
+
+var_dump($curso);
+
+echo isset($curso);
+
+unset($curso);
+
+echo $curso;
+
+echo isset($curso);
+
+
+$nome = "Tiago";
+$sobreNome = "Brito";
+
+echo $nome." ".$sobreNome;
+
+$dataNascimento = new DateTime();
+
+echo"<br>";
+var_dump($dataNascimento);
+echo"<br>";
+var_dump($ano);
+echo"<br>";
+$arquivo = fopen("C:\xampp\htdocs\PhpHcode\index.php", "r");
+var_dump($arquivo);
+echo"<br>";
+
+$hoje = date("d/m/y");
+echo ($hoje);*/
+
+$nome = (int)$_GET["b"];
+
+//var_dump($nome);
+echo "<br>";
+
+//$ip = $_SERVER["REMOTE_ADDR"];
+//echo $ip;
+
+//$ip = $_SERVER["SCRIPT_NAME"];
+//echo $ip;
+
+$nome = "Tiago Brito";
+echo $nome;
+
+function ExibeNome(){
+    
+    global $nome;
+    $nome = "Luiz Carlos";
+}
+
+ExibeNome();
+echo $nome;
 
 ?>
